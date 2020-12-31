656e689061ccc427f80f72ae061c635fe5a7a66b
Guest <guest-jykat9@h28593.sgb.skir.kr>
2020-12-31 23:42:52 +0900

    d

--- recursive-filling-bitwise.js
+++ recursive-filling-bitwise.js
@@ -35,10 +35,32 @@ for (i = 0; i < 9; i++) {
 
 /*
 빈 칸 위치(i,j), 채울 숫자(bit 형식)를 받는다.
-빈 칸을 포함하는 section을 계산한다.
-해당 section의 빈 칸에 대해 채우기 연산을 한다.
+sudokuMatrix의 해당 위치를 입력 숫자로 변경한다.
+빈 칸을 포함하는 section의 모든 빈 칸에 대해
+해당 숫자를 지우기 연산 (a & ~(1 << n))
 */
 
+function addNumber(n, i, j) {       // n은 0000100000 이런 형식
+    let startK;
+    let startL;
+    sudokuMatris[i][j] = n;
+    // row
+    for (k = 0; k < 9; k++) {
+        sudokuMatrix[i][k] = sudokuMatrix[i][k] & ~n;
+    }
+    // column
+    for (k = 0; k < 9; k++) {
+        sudokuMatrix[k][j] = sudokuMatrix[k][j] & ~n;
+    }
+    // block
+    startK = 3*Math.floor(i/3);
+    startL = 3*Math.floor(j/3);
+    for (k = startK; k < startK + 3; k++) {
+        for (l = startL; l <startL + 3; l++) {
+            sudokuMatrix[k][l] = sudokuMatrix[k][l] & ~n;            
+        }
+    }
+}
 
 
 
@@ -78,8 +100,7 @@ function getInput() {
 해당하는 요소가 포함된 section에 대해 같은 연산을 재귀적으로 실행.
 종료 조건은 그냥 채울 수 있는 요소가 없을 경우.*/
 
-/*
-functino recursiveFilling(section1)
+/* functino recursiveFilling(section1)
 single N 검사
 요소 있으면
     포함 section 계산, 포함 section에 대해 재귀연산
@@ -91,30 +112,59 @@ single B 검사
 /* single N
 section의 모든 빈칸에 대해
 채워지지 않았고(bit 마지막 자리가 1), 나머지 중 하나만 1일 때 추출
-k&(k-1)을 하면 자리가 하나일 경우 0이 나온다.
+(k-1)&(k-2)을 하면 자리가 하나일 경우 0이 나온다.
 
 해당 빈 칸의 숫자를 k
 if k & 1 > 0
-    if k&(k-1) > 0
+    if k&(k-1) === 0
         채울 수 있음!
+*/
+
+/* single B
+
+-> 까다로움.
+내가 생각한 방식은 연산자 *: 0*0=1*1=0, 0*1=1*0=1로 두고(xor), 1이 두 번 나올 경우 아예 취급하지 않게끔.
+mask = 1111111111
+a = 1111111111
+FOR i++
+    a = a ^ (빈칸i & mask)
+    mask = a 
+
+A = 2047 (=1111111111)
+FOR 처음 빈 칸 to 마지막 빈 칸 DO
+    A ? (A는 single B에 해당하는 원소들의 집합이 됨)
+IF A !== 0 DO
+    (위치 및 single B로 나온 숫자 찾기)
+    FOR 처음 빈 칸 to 마지막 빈 칸 DO
+        IF 해당 빈 칸 bit & A !==0 DO   (A에 속한 원소가 있는 칸 찾기)
+            위치, (해당 빈 칸 & A) 값 채우고   (해당 칸 & A는 single B로 나온 원소)
+            
 */
 
 
 
 function recursiveFilling(section) {
     let k;
+    let a;
     // 먼저 section 종류 구분
     if (section < 9) {    // row
         //single N
         for (i = 0; i < 9; i++) {
             k = sudokuMatrix[section][i];
-            if (k & 1 > 0) {    // 채워지지 않음.
-                if (k&(k-1) > 0) {
+            if (k & 1 !== 0) {    // 채워지지 않음.
+                if ( (k >> 1) & ((k >> 1) - 1) === 0) {                         // k = 0000010001 이런 형식
+                    addNumber(k-1, section, i);
                     recursiveFilling(section);      // row(itself)
                     recursiveFilling(i + 9);        // column
                     recursiveFilling();             
                 }
             }
+        }
+        // single B
+        a = 2047;
+        for (i = 0; i < 9; i++) {
+
+
         }
     }
     else if (section < 18) {    // column
